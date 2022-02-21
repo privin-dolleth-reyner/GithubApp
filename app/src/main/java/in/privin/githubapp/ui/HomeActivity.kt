ea6f12cf0a5d7bbca4c49cde48f224abae0a803f
privindolleth <privin@okcredit.in>
2022-02-21 09:17:11 +0530

    Home activity loads the list

---
+++ app/src/main/java/in/privin/githubapp/ui/HomeActivity.kt
@@ -0,0 +1,47 @@
+package `in`.privin.githubapp.ui
+
+import `in`.privin.githubapp.App
+import `in`.privin.githubapp.databinding.ActivityMainBinding
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+
+class HomeActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+
+    private val viewModel: HomeViewModel by lazy {
+        HomeViewModel(App.getInstance().githubRepository)
+    }
+
+    lateinit var binding: ActivityMainBinding
+
+    private val rvAdapter = GithubPrListAdapter(this,listOf())
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        observeData()
+    }
+
+    private fun observeData() {
+        viewModel.prList.observe(this) {
+            rvAdapter.updateData(it)
+        }
+    }
+
+    private fun initRecyclerView(){
+        binding.rvGithubPr.adapter = rvAdapter
+        binding.rvGithubPr.layoutManager = LinearLayoutManager(this)
+        binding.rvGithubPr.addItemDecoration(ListItemDecoration(10))
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initRecyclerView()
+        viewModel.getClosedPrList()
+    }
+}
